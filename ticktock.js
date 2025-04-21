3ad7e165a15015cb0183e8ff8f33c1d9a7dbcb60
mbahramii <bahramiimohammad92@gmail.com>
2025-04-21 18:42:23 +0330

    feat(timer): implement startCountdown, start, stop, and remove functionalities in JS

---
+++ ticktock.js
@@ -0,0 +1,119 @@
+    // toggle between page  
+    function showPage(page) {   
+        const pages = ['stopwatchpage' , 'clockpage' , 'timerrpage' , 'alarmpage']
+
+        pages.forEach(id => {
+            document.getElementById(id).style.display = (id === page + 'page') ? 'block' : 'none' ;  
+
+        })}
+       
+       //  stopwatch section    
+        let time = 0 ;
+        let timer = null ; 
+
+        const stopwatchElement = document.getElementById("stopwatch");
+
+        const startStopButton =  document.getElementById('startstop');
+        
+        // formdata time  
+        const formatdate = (seconds) => {
+
+            hours = String(Math.floor(seconds / 3600 )).padStart(2, '0')
+            minute = String(Math.floor((seconds % 3600 )/60)).padStart(2, '0')
+            sec = String(Math.floor(seconds % 60 )).padStart(2, '0')
+
+            return `${hours}:${minute}:${sec}`
+        }
+
+        //  Condition for start, stop, and continue
+        const startTimer =()=> {
+
+            if (timer) {
+                clearInterval(timer)
+                timer = null ;
+                startStopButton.textContent = 'continue'
+            }
+            else {
+                timer = setInterval (() =>{
+                    time++
+                    stopwatchElement.textContent = formatdate(time)
+                } , 1000)
+                startStopButton.textContent = 'stop'
+            }
+        }
+
+        // reset stopwatch
+        const resetTimer = () =>{
+            clearInterval(timer)
+            time  = 0 ;
+            timer = null;
+            stopwatchElement.textContent = formatdate(time)
+
+            startStopButton.textContent = 'start'
+        }
+
+
+         //  timer section           
+        let seconds = 86399 ; 
+
+        // countinue & remove button 
+        const crButton = document.getElementById('crButton')
+        const stoptimer= document.getElementById('stoptimer')
+
+        const timerElement = document.getElementById('timerr') 
+
+        const formatdatetimer =(seconds)=>{
+            const hours = String(Math.floor(seconds / 3600 )).padStart(2, '0')
+            const minute = String(Math.floor((seconds % 3600 )/60)).padStart(2, '0')
+            const sec = String(seconds % 60 ).padStart(2, '0')
+            
+            
+            return `${hours}:${minute}:${sec}`
+        }
+        
+        const startCountdown= () =>{  
+    
+            if(timer){
+                clearInterval(timer)
+                timer = null 
+                stoptimer.textContent = 'countinue'
+            } else {
+                    timer = setInterval(() =>{
+                    seconds--
+                    stoptimer.textContent = 'stop'
+                    timerElement.textContent = formatdatetimer(seconds) 
+
+                },1000)};
+            }
+
+            
+            document.getElementById('crButton').addEventListener('click' , function(){
+                let buttonstarttimer = document.getElementById('crButton')
+                let buttonsContaine = document.getElementById('buttonsContaine')
+            if(buttonsContaine.style.display === 'none' || buttonsContaine.style.display === '' ){
+            buttonsContaine.style.display = 'block';
+            buttonstarttimer.style.display= 'none' ;
+            }else {
+            buttonsContaine.style.display = 'none'
+            buttonstarttimer.style.display= 'block' ;
+        }      
+        })
+        
+        const removetimer = () => {
+            clearInterval(timer)
+            seconds = 86399
+            timer = null
+            timerElement.textContent = formatdatetimer(seconds)
+            crButton.textContent = 'start'
+
+            let buttonstarttimer = document.getElementById('crButton')
+            let buttonsContaine = document.getElementById('buttonsContaine')
+            if(buttonsContaine.style.display === 'block' ){
+            buttonsContaine.style.display = 'none';
+            buttonstarttimer.style.display= 'block' ;
+            }else {
+            buttonsContaine.style.display = 'none'
+            buttonstarttimer.style.display= 'block' ;
+        }  
+        }    
+        
